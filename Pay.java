4649cfbdcc1fb334d9e13df2a21211961d87f793
Craig Sullivan <craigsullivan29@gmail.com>
2017-10-12 19:30:02 +0100

    Add files via upload

---
+++ Pay.java
@@ -0,0 +1,70 @@
+/*
+Pay.java
+@ author C Sullivan
+Lab Exercise
+14th Feb 2017
+*/
+
+public class Pay{
+	private double basicPay;
+	private double basicHours;
+	private double overtimeHours;
+	private double basicTotalPay;
+	private double totalPay;
+	private double otTotalPay;
+
+	public Pay(){
+		basicPay=0.00;
+		basicHours=0.00;
+		overtimeHours=0.00;
+		totalPay=0.00;
+		basicTotalPay=0.00;
+		otTotalPay=0.00;
+
+	}
+
+
+	public void setBasicPay(double basicPay){
+		this.basicPay=basicPay;
+	}
+
+	public void setBasicHours(double basicHours){
+		this.basicHours=basicHours;
+	}
+
+	public void setOvertimeHours(double overtimeHours){
+		this.overtimeHours=overtimeHours;
+	}
+
+
+
+	public void compute(){
+		basicTotalPay=(basicPay*basicHours);
+		otTotalPay=(overtimeHours)*(basicPay*1.5);
+		totalPay=(basicPay*basicHours)+(basicPay*1.5*overtimeHours);
+	}
+
+
+
+	public double getBasicTotalPay(){
+		return basicTotalPay;
+	}
+
+	public double getOtTotalPay(){
+		return otTotalPay;
+	}
+
+	public double getTotalPay(){
+		return totalPay;
+
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ PayApp.java
@@ -0,0 +1,58 @@
+/*
+PayApp.java
+@ author C Sullivan
+Lab Exercise
+14th Feb 2017
+*/
+
+import javax.swing.*;
+public class PayApp{
+	public static void main(String args[]){
+
+		double basicPay;
+		double basicHours;
+		double overtimeHours;
+		double basicTotalPay;
+		double otTotalPay;
+		double totalPay;
+
+		Pay myPay;
+		myPay = new Pay();
+
+		basicPay=Double.parseDouble(JOptionPane.showInputDialog(null, "What is your basic rate of pay?"));
+		myPay.setBasicPay(basicPay);
+
+		basicHours=Double.parseDouble(JOptionPane.showInputDialog(null, "How many basic hours work did you do"));
+		myPay.setBasicHours(basicHours);
+
+		overtimeHours=Double.parseDouble(JOptionPane.showInputDialog(null, "How many over-time hours work did you do"));
+		myPay.setOvertimeHours(overtimeHours);
+
+
+			myPay.compute();
+
+			basicTotalPay=myPay.getBasicTotalPay();
+			JOptionPane.showMessageDialog(null, "The total amount you earned for your basic hours is €"+basicTotalPay);
+
+			otTotalPay=myPay.getOtTotalPay();
+			JOptionPane.showMessageDialog(null, "The total amount you earned for your over-time hours is €"+otTotalPay);
+
+			totalPay=myPay.getTotalPay();
+			JOptionPane.showMessageDialog(null, "The total amount you earned is €"+totalPay);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+	}
